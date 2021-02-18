9a207841dc13c6488b889870b73caeca0f5fc686
Hugo Posca <hugo@gruntwork.io>
2021-02-18 10:36:00 -0800

    Verifying SHA before proceding
    Not fail proof but at least we are now verifying that the SHA matches.

--- install_latest_golang.sh
+++ install_latest_golang.sh
@@ -3,11 +3,26 @@
 # Installs the latest version of golang
 
 DOWNLOAD_PAGE="https://golang.org/dl/"
+
+tmp_page=$(mktemp)
+curl -s "$DOWNLOAD_PAGE" -o "$tmp_page"
+
 # Get current version of Go for 64-bit Linux
-LATEST_VERSION=$(curl -s "$DOWNLOAD_PAGE" | grep linux-amd64 | grep 'download downloadBox' | cut -d'"' -f4 | cut -d'/' -f3)
+LATEST_VERSION=$(grep linux-amd64 "$tmp_page" | grep 'download downloadBox' | cut -d'"' -f4 | cut -d'/' -f3)
+
+SHA=$(grep -A10 "$LATEST_VERSION" "$tmp_page" | grep "<td><tt>" | sed 's/<[^>]*>//g' | tr -d ' ')
 
 # Download Go
 wget "$DOWNLOAD_PAGE$LATEST_VERSION"
+
+sha_result=$(sha256sum "$LATEST_VERSION" | cut -d' ' -f1)
+echo "$sha_result"
+if [[ "$SHA" != "$sha_result" ]]; then
+  echo "SHA doesn't match, aborting..."
+  exit 1
+fi
+
+echo "SHA matched, proceding"
 
 # Unpack file to /usr/local
 sudo tar -C /usr/local -xzf "$LATEST_VERSION"
