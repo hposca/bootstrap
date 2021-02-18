7364d459066543b54c863aec5774bbfd2be44427
Hugo Posca <hugo@gruntwork.io>
2021-02-18 11:26:25 -0800

    Uncommenting

--- install_tf_tools.sh
+++ install_tf_tools.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# echo "Installing tfenv"
-# git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-# ln -s ~/.tfenv/bin/* ~/.local/bin
-#
-# echo "Installing tgenv"
-# git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
-# ln -s ~/.tgenv/bin/* ~/.local/bin
+echo "Installing tfenv"
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+ln -s ~/.tfenv/bin/* ~/.local/bin
+
+echo "Installing tgenv"
+git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
+ln -s ~/.tgenv/bin/* ~/.local/bin
 
 echo "Installing pre-commit"
 pip3 install --user pre-commit
