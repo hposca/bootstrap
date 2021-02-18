2097320bfd208affa1ec95ecb7b02dc008e4ea3d
Hugo Posca <hugo@gruntwork.io>
2021-02-18 12:09:29 -0800

    Install kubectx and kubens

--- install_tf_tools.sh
+++ install_tf_tools.sh
@@ -12,3 +12,28 @@ ln -s ~/.tgenv/bin/* ~/.local/bin
 
 echo "Installing pre-commit"
 pip3 install --user pre-commit
+
+echo "Installing kubectx and kubens"
+tmp_page=$(mktemp)
+curl -s https://api.github.com/repos/ahmetb/kubectx/releases/latest -o "$tmp_page"
+addresses=$(jq -r '.assets[] | select(.name | endswith("linux_x86_64.tar.gz")) | {url: .browser_download_url, name: .name}' "$tmp_page")
+
+echo "$addresses"
+
+echo "Installing kubens"
+kubens_filename=$(echo "$addresses" | jq -r 'select(.name | contains("kubens")) | .name')
+kubens_url=$(echo "$addresses" | jq -r 'select(.name | contains("kubens")) | .url')
+echo "$kubens_filename"
+echo "$kubens_url"
+
+wget "$kubens_url"
+tar -xvf "$kubens_filename" -C ~/bin/ kubens
+
+echo "Installing kubectx"
+kubectx_filename=$(echo "$addresses" | jq -r 'select(.name | contains("kubectx")) | .name')
+kubectx_url=$(echo "$addresses" | jq -r 'select(.name | contains("kubectx")) | .url')
+echo "$kubectx_filename"
+echo "$kubectx_url"
+
+wget "$kubectx_url"
+tar -xvf "$kubectx_filename" -C ~/bin/ kubectx
